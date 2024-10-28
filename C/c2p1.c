8ac6711608ca607a58fd04b274b6c4d4044c10ee
Blue <120330097+BlueDragon320@users.noreply.github.com>
2024-10-28 20:56:38 +0530

    Add files via upload

---
+++ C/c2p1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+    int volume = a*a*a;
+    printf("The volume of cube is %d\n", volume);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
