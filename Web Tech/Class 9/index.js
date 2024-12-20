118457da466b188947bf46cba8c685cea3d3c88e
Blue <120330097+BlueDragon320@users.noreply.github.com>
2024-12-20 22:31:17 +0530

    Add files via upload

---
+++ Web Tech/Class 9/index.js
@@ -0,0 +1,12 @@
+let num1= 10;
+let num2= 20;
+let num3= 30;
+let num4= 40;
+let num5= 50;
+let avg = (num1 + num2 + num3 + num4 + num5)/5;
+let sum = (num1 + num2 + num3 + num4 + num5);
+let sub = (num1 - num2 - num3 - num4 - num5);
+console.log(avg);
+console.log(sum);
+console.log(sub);
+
